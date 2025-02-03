e352220d03baa28657005015cf328d44c9fe6e8a
paulotassi <133156624+paulotassi@users.noreply.github.com>
2025-02-02 21:28:50 -0500

    finished script

--- Assets/Script/DistanceChanger.cs
+++ Assets/Script/DistanceChanger.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DistanceChanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera _camera;
+    public float modifier;
+    public float maxDistance;
+    public float minRateAmount;
+    public float maxParticleSpeed;
     void Start()
     {
-        
+        _camera = FindFirstObjectByType<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        modifier = Mathf.Clamp01((DistanceChecker(this.gameObject.transform, _camera.transform))/maxDistance);
+        var emissionValue = this.gameObject.GetComponent<ParticleSystem>().emission;
+        emissionValue.rateOverTime = Mathf.Clamp((minRateAmount/Mathf.Pow(modifier,4)), minRateAmount, 7500);
+        var velocityValue = this.gameObject.GetComponent<ParticleSystem>().velocityOverLifetime;
+        velocityValue.speedModifier = maxParticleSpeed/modifier;
+
+
+    }
+
+    float DistanceChecker(Transform object1, Transform object2)
+    {
+
+        return Vector3.Distance(object1.position, object2.position);
     }
 }
